56182e29ed9593ea60cfeed7ba9b1297efc0d876
@Vlad <vlad.owl4@gmail.com>
2017-05-12 14:45:54 +0300

    Fix routes

--- app/routes.js
+++ app/routes.js
@@ -3,32 +3,15 @@ module.exports = function (app, passport) {
     var categorys = require('./controllers/category');
     var products = require('./controllers/product');
 
-    //====== HOME PAGE ======
-    app.get('/',  function (req, res) {
-        res.render('index.ejs');
-    });
-
 
     //====== LOGIN ======
     app.route('/login')
-        .get(function (req, res) {
-            res.render('index.html');
-        })
-        // process the login form
         .post(
             passport.authenticate('local'),function (req, res) {
                 res.json(req.body.username);
             });
 
 
-    //====== PROFILE SECTION ======
-
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('content.ejs', {
-            // get the user out of session and pass to template
-            user : req.user
-        });
-    });
     //====== LOGOUT ======
     app.get('/logout', function (req, res) {
         req.logout();
@@ -57,12 +40,6 @@ module.exports = function (app, passport) {
         .put(products.update)
         .delete(products.remove);
 
-
-    //====== 404 =======
-    app.get('/*', function (req, res) {
-        res.status(404);
-        res.render('404.ejs');
-    })
 };
 
 function isLoggedIn(req, res, next){
